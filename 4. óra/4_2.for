8fa96d2980277c50287fee4e224632adb7f11776
Vitkolczi Dániel <90444730+Feekyy@users.noreply.github.com>
2022-09-27 14:33:48 +0200

    Add files via upload

---
+++ 4. óra/4_2.for
@@ -0,0 +1,2 @@
+\select@language {magyar} \contentsline {forras}{\numberline {1}{\ignorespaces ez a felirat}}{1}{}%
+\select@language {magyar} \contentsline {forras}{\numberline {2}{\ignorespaces pray for salvation}}{2}{}%
